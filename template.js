9375981679199bfe373d7f8e73a0aba0785d0f9b
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-20 02:18:11 +0600

    fifth commit

---
+++ template.js
@@ -0,0 +1,10 @@
+const singleLine = 'This single line';
+console.log( singleLine);
+const singleLine2 = "This is SEcond line";
+console.log(singleLine2);
+// backtik
+const multiLine = `This first line
+this is seconde line
+this is third line
+this is forth line`;
+console.log(multiLine);
