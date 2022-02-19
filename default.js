d88c983d6b99f3f41bb3091b45a7835ae82c0265
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-20 02:03:01 +0600

    last commit

--- default.js
+++ default.js
@@ -1,6 +1,14 @@
-function add(num1,num2=2) {
-    const total = num1 + num2;
-    return total;
+// function add(num1,num2=2) {
+//     const total = num1 + num2;
+//     return total;
+// }
+// const results = add(10);
+// console.log(results);
+
+
+function sub(num1,num2 = 5){
+    const subtarction = num1 - num2;
+    return subtarction
 }
-const results = add(10);
+const results = sub (10,8);
 console.log(results);
