0e3c1cc1b59665456fe1fe3984a102c6d1c1a7e0
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-20 00:46:03 +0600

    second commit

--- basic.js
+++ basic.js
@@ -1,2 +1,19 @@
 // variable declare
 var deposit = 400;
+// condition
+if(deposit > 400){
+
+}
+if (deposit == 400) {
+    
+}
+if (deposit < 400) {
+    
+}
+if (deposit != 400) {
+    
+}
+// array
+const numbers = [23,45,56,78,90,22,43];
+const numberCount = numbers.lenght;
+console.log(numberCount);
